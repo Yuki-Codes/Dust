83cff74834e508a3bcc618cc39af9971233a98cf
Yuki Walsh <Yuki.Walsh@Outlook.com>
2026-08-11 20:08:25 +1000

    game display progress

---
+++ Dust/Views/GamesView.swift
@@ -0,0 +1,18 @@
+//
+//  GamesView.swift
+//  Dust
+//
+//  Created by Yuki Walsh on 2026-08-11.
+//
+
+import SwiftUI
+
+struct GamesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GamesView()
+}
